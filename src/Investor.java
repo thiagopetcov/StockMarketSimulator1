0770c7d1f97023563cfd5aef4450c40cae1ac0f9
thiagopetcov <thiago.petcov@gmail.com>
2020-05-15 21:51:03 +0100

    Investor

---
+++ src/Investor.java
@@ -0,0 +1,24 @@
+public class Investor {
+
+	//Investor Attributes 
+		private int id;
+		private int budget;
+		
+		//Getters and Setters 
+		public double getBudget(){
+			return budget;
+		}
+		
+		public void setBudget( int budget){
+			this.budget = budget;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+	}
