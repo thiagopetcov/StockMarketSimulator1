1ab6603f88b7edf82ab81e5515dd764b2723a560
thiagopetcov <thiago.petcov@gmail.com>
2020-05-15 21:47:04 +0100

    Company

---
+++ src/Company.java
@@ -0,0 +1,34 @@
+public class Company {
+	
+		//Company Attributes 
+		private int id;
+		private int shares;
+		private int price;
+		
+		//Getters and Setters 
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public int getShares() {
+			return shares;
+		}
+
+		public void setShares(int shares) {
+			this.shares = shares;
+		}
+
+		public int getPrice() {
+			return price;
+		}
+
+		public void setPrice(int price) {
+			this.price = price;
+		}
+
+	}
+
