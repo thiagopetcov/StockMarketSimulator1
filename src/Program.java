7b9e6cc061a00a0c67dceb7327e10df0eb2ba16d
thiagopetcov <thiago.petcov@gmail.com>
2020-05-16 12:27:40 +0100

    Arrays CI

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Program {
+
+	public static void main(String[] args) {
+		 //Company array it will store 100 companies
+		ArrayList<Company> companies = new ArrayList<>();
+		//Investor array it will store 100 investors 
+		ArrayList<Investor> investors = new ArrayList<>();
+	
