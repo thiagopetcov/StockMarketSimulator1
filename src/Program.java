a9fa75fa42515e06807b6b496446cdf8e215adc9
thiagopetcov <thiago.petcov@gmail.com>
2020-05-16 12:37:25 +0100

    Random class

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Program {
 
@@ -8,4 +9,34 @@ public class Program {
 		//Investor array it will store 100 investors 
 		ArrayList<Investor> investors = new ArrayList<>();
 	
-		//test array
+Random random = new Random();
+		
+		for (int i = 1; i < 101; i++) {
+			int min = 500, max = 1000, minPrice = 10, maxPrice = 100;//min = 1000, maxBudget = 10000
+			Company company = new Company();
+			company.setId(i);
+			company.setShares(min + random.nextInt(max));
+			company.setPrice(minPrice + random.nextInt(maxPrice));
+			//company.setBudget(minBudget + random.nextInt(maxBudget));
+			companies.add(company);
+		}
+		
+		for (Company company : companies) {
+			System.out.println("Company ID: " + company.getId() + " Shares: " + company.getShares() + " Price: " + company.getPrice());
+		}
+		
+		for (int i = 1; i < 101; i++) {
+			Investor investor = new Investor();
+			investor.setId(i);
+			investors.add(investor);
+		}
+		
+		for (Investor investor : investors) {
+			System.out.println("Investor ID: " + investor.getId());
+		}
+		
+		
+		System.out.println("RANDOM: " + random.nextInt(100));
+	}
+
+}
